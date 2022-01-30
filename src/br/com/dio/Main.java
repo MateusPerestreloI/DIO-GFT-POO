0e510d92b9af37c511d63cc3ad2c46d3a7be7831
Mateus I <mateuspi.0398@gmail.com>
2022-01-30 18:07:00 -0300

    Modificando o Projeto
    Modificando o projeto

---
+++ src/br/com/dio/Main.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.models.Curso;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Curso curso1 = new Curso();
+	}
+
+}
